e7713ae288d29a8bfa925f1eebe8fb2b027f6dec
davidauza.engineer <davidauza01@gmail.com>
2020-05-28 15:23:33 -0500

    Configure JSON Web Tokens

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module Rails6JwtAuth
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # JWT configuration
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
