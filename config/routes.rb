a7fd1ae3ac55defc715cd74c7508fa994ae61065
davidauza.engineer <davidauza01@gmail.com>
2020-05-28 15:58:30 -0500

    Implement User creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/api/auth/signin', to: 'authentication#authenticate'
+  post '/api/auth/signup', to: 'users#create'
+  resources :users, only: [:create, :show]
 end
